35c1269a9f1501e170aa7aa554a67716ce029bd9
Chonlathorn Kwankajornkiet <kam.chonlathorn@gmail.com>
2017-05-22 22:53:04 +0700

    pos: add one more weight at input layer

--- pos_tag/tagger_model.py
+++ pos_tag/tagger_model.py
@@ -25,13 +25,16 @@ with graph.as_default():
     embedding = tf.placeholder("float", [vocabulary_size, n_input])
 
     with tf.device('/cpu:0'):
+        generated_embedding = tf.Variable(tf.random_uniform([vocabulary_size, embedding_dim], minval=-0.1, maxval=0.1))
         hidden_weights = tf.Variable(tf.truncated_normal([2 * n_hidden_1, n_hidden_2], stddev=1.0/sqrt(n_hidden_2)))
         hidden_biases = tf.Variable(tf.zeros([n_hidden_2]))
 
         out_weights = tf.Variable(tf.truncated_normal([n_hidden_2, n_classes], stddev=1.0/sqrt(n_classes)))
         out_biases = tf.Variable(tf.zeros([n_classes]))
 
-        x_input = tf.nn.embedding_lookup(embedding, x)
+        x1 = tf.nn.embedding_lookup(embedding, x)
+        x2 = tf.nn.embedding_lookup(generated_embedding, x)
+        x_input = x1 + x2
         x_input = tf.transpose(x_input, [1, 0, 2])
         x_input = tf.reshape(x_input, [-1, n_input])
         x_input = tf.split(x_input, n_steps, 0)
@@ -47,7 +50,7 @@ with graph.as_default():
         rnn_outputs, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x_input,
                                                      dtype=tf.float32, sequence_length=sentence_len)
 
-        hidden_outputs = [tf.nn.tanh(tf.matmul(rnn_outputs[idx], hidden_weights) + hidden_biases)
+        hidden_outputs = [tf.nn.relu(tf.matmul(rnn_outputs[idx], hidden_weights) + hidden_biases)
                           for idx in range(n_steps)]
 
         dropped_outputs = tf.nn.dropout(hidden_outputs, dropout_prob)
@@ -57,7 +60,7 @@ with graph.as_default():
         final_output = tf.transpose(predicted_outputs, perm=[1, 0, 2])
 
         ce_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=training_outputs, labels=y_label))
-        l2_loss = tf.nn.l2_loss(hidden_weights) + tf.nn.l2_loss(out_weights)
+        l2_loss = tf.nn.l2_loss(generated_embedding) + tf.nn.l2_loss(hidden_weights) + tf.nn.l2_loss(out_weights)
         loss = tf.reduce_mean(ce_loss + (l2_beta * l2_loss))
         optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate).minimize(loss)
 
